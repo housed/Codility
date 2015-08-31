85e547dcdb4c099a250fabc298f249a8e44532ef
Dylan House <housed@users.noreply.github.com>
2015-08-30 18:25:09 -0700

    Update TapeEquilibrium.cpp

--- Lesson_1/TapeEquilibrium.cpp
+++ Lesson_1/TapeEquilibrium.cpp
@@ -1,10 +1,15 @@
-// 100 % : https://codility.com/demo/results/demoU53ER4-UQ7/
+// 100%, O(N): https://codility.com/demo/results/demoR6PXA2-EQJ/
 
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 int solution(vector<int> &A) {
+    if (A.size() == 0) {
+        return 0;   
+    }
+    
     if (A.size() == 1) {
         return A[0];   
     }
@@ -13,24 +18,26 @@ int solution(vector<int> &A) {
         return abs(A[0] - A[1]);   
     }
     
-    int left_sum = A[0], 
+    int left_sum = 0,
         right_sum = 0;
-    for (unsigned i = 1; i < A.size(); i++) {
-        right_sum += A[i];   
+    for (unsigned i = 0; i < A.size(); i++) {
+        right_sum += A[i];
     }
     
     unsigned difference = 0, 
              minimal_difference = 9999;
     
-    for (unsigned j = 1; j < A.size(); j++) {
+    for (unsigned j = 0; j < A.size() - 1; j++) {
+        left_sum += A[j];
+        right_sum -= A[j];
+       
         difference = abs(left_sum - right_sum);
+        
+        cout << left_sum << ' ' << right_sum << ' ' << difference << '\n';
         
         if (difference < minimal_difference) {
             minimal_difference = difference;   
         }
-        
-        left_sum += A[j];
-        right_sum -= A[j];
     }
     
     return minimal_difference;
